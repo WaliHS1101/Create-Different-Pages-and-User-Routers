c39ddfe628200f5794702ca9608595b490454d4f
WaliHS1101 <walih1101@gmail.com>
2024-01-02 17:46:04 +0500

    Add Profile Page

---
+++ src/Components/ProfilePae.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ProfilePae() {
+    return (
+        <>
+            <div className='container'>
+
+            </div>
+        </>
+    )
+}
+
+export default ProfilePae
